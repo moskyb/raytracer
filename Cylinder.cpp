007c921d71c490eb108fa0c9374914c9b24e6795
Ben Moskovitz <ben@mosk.nz>
2016-06-08 13:58:28 +1200

    Filled out Cylinder's methods

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -5,28 +5,25 @@
 * Cylinder's intersection method.  The input is a ray (pos, dir).
 */
 float Cylinder::intersect(Vector pos, Vector dir) {
-  Vector vdif = pos - center;
   float a = pow(dir.x, 2) + pow(dir.z, 2);
   float b = (dir.x * (pos.x - origin.x)) + (dir.z *(pos.z - origin.z));
   float c = pow((pos.x - origin.x), 2) + pow((pos.z - origin.z), 2) - pow(radius, 2);
 
   float desc = pow(b, 2) - 4 * a * c;
 
-  if (fabs(desc) > 0.001) {
-    float t1 = -b - sqrt(desc);
-    float t2 = -b + sqrt(desc);
+  if (fabs(desc) > 0.001 || desc < 0.0) {
+    return -1.;
+  }
+  float t1 = -b - sqrt(desc);
+  float t2 = -b + sqrt(desc);
+  if(fabs(t1) < 0.001 )
+  {
+    if (t2 > 0) return t2;
+    else t1 = -1.0;
+  }
+  if(fabs(t2) < 0.001 ) t2 = -1.0;
 
-    if(fabs(t1) < 0.001 ) {
-      if (t2 > 0) return t2;
-      else t1 = -1.0;
-    } else {
-      return -1.
-    }
-    if(fabs(t2) < 0.001 ){
-      t2 = -1.0;
-    }
-
-    return (t1 < t2)? t1: t2;
+	return (t1 < t2)? t1: t2;
 }
 
 /**
@@ -35,7 +32,8 @@ float Cylinder::intersect(Vector pos, Vector dir) {
 */
 Vector Cylinder::normal(Vector p)
 {
-    Vector n = p - center;
+    Vector n = p / radius;
+    n.y = 0;
     n.normalise();
     return n;
 }
